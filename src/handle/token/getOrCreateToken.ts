16119c5f5fe7ddff6a69404e942931bcc14ee256
hailiting <hailiting@yeah.net>
2022-09-05 16:13:32 +0800

    add

--- src/handle/token/getOrCreateToken.ts
+++ src/handle/token/getOrCreateToken.ts
@@ -2,6 +2,8 @@ import { CommonHandlerContext } from "@subsquid/substrate-processor";
 import { Store } from "@subsquid/typeorm-store";
 import { Token } from "../../model";
 import * as ERC20 from "../../abis/ERC20";
+import { convertTokenToDecimal } from "../../utils/number";
+import { ZERO_BD } from "../../config/consts";
 
 export async function getOrCreateToken(
   ctx: CommonHandlerContext<Store>,
@@ -13,11 +15,23 @@ export async function getOrCreateToken(
     const name = await erc20.name();
     const symbol = await erc20.symbol();
     const decimals = await erc20.decimals();
+    const totalSupply = await erc20.totalSupply();
+
     token = new Token({
       id: address.toLowerCase(),
       symbol,
       name,
+      totalSupply: convertTokenToDecimal(
+        totalSupply.toBigInt(),
+        decimals
+      ).toString(),
       decimals,
+      derivedNative: ZERO_BD.toString(),
+      tradeVolume: ZERO_BD.toString(),
+      tradeVolumeUSD: ZERO_BD.toString(),
+      untrackedVolumeUSD: ZERO_BD.toString(),
+      totalLiquidity: ZERO_BD.toString(),
+      txCount: 0,
     });
     await ctx.store.save(token);
   }
